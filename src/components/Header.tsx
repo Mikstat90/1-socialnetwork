b712ce2ad70cd3b0f1ae57b4c7e4f6241d41a132
Dzmitry Tsitarau <mikstat90@gmail.com>
2023-09-25 06:48:04 +0200

    components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://images.secretlab.co/theme/common/tw_chair_bg.svg' alt='witcher-1'/>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://static.bandainamcoent.eu/high/the-witcher/the-witcher-3-wild-hunt/00-page-setup/tw3-new-header-mobile.jpg"
+                    alt="witcher-2"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
